a7e02b533f70c3563842dc2ead8ee73e490ba76a
konchan <konno.katsuyuki@nifty.com>
2012-09-02 00:05:33 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wasabi::Application.config.secret_token = '30de5a662c2362f201165e5effebb7a4dd3162bf47f989062d134fe9997cd74f23e94cc7edabd220f6b78bb2f253de9a03d3fb833c78a4aaab81bbc333be3381'
